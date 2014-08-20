0ab175725a91be3f514b126b758d22e119cc3748
David Ellis <davidkellis@gmail.com>
2014-08-19 23:03:49 -0500

    Downloading Treasury Rate data from www.treasurydirect.gov instead of Yahoo!.

--- download_data.rb
+++ download_data.rb
@@ -1,11 +1,10 @@
-require_relative 'yahoofinance'
-
 require 'pp'
 require 'csv'
-require 'nokogiri'
 require 'open-uri'
 require 'date'
 require 'fileutils'
+require 'json'
+require 'bigdecimal'
  
 MonthToMonthCode = (1..12).zip(['F','G','H','J','K','M','N','Q','U','V','X','Z']).to_h    # {1=>"F", 2=>"G", 3=>"H", 4=>"J", 5=>"K", 6=>"M", 7=>"N", 8=>"Q", 9=>"U", 10=>"V", 11=>"X", 12=>"Z"}
 MonthCodeToMonth = MonthToMonthCode.invert
@@ -42,16 +41,197 @@ rescue => e
   puts e.message
 end
 
+TreasuryBill = Struct.new(:auction_date, :issue_date, :maturity_date, :face_value, :purchase_price, :high_price, :low_price, :high_discount_rate, :low_discount_rate, :maturity_in_days)
+
 def save_13_week_treasury_bill_rate
-  start_date = Date.new(2005, 1, 1)
-  end_date = Today
+  # download from Yahoo!
+  # start_date = Date.new(2005, 1, 1)
+  # end_date = Today
+  # lines = ["Date,Value"]
+  # YahooFinance.get_historical_quotes("^IRX", start_date, end_date) do |row|
+  #   # each row is of the form: [date (yyyy-mm-dd), open, high, low, close, volume, adj-close]
+  #   date, open, high, low, close, volume, adj_close = *row
+  #   lines << "#{date},#{high.to_f / 100.0}"
+  # end
+  # File.write(TreasuryBillRateFilePath, lines.join("\n"))
+  
+  # download from US Treasury
+  url = 'http://www.treasurydirect.gov/TA_WS/securities/jqsearch?format=json&securityTypeoperator=and&filtervalue0=Bill&filtercondition0=EQUAL&filteroperator0=1&filterdatafield0=securityType&securityTermoperator=and&filtervalue1=13&filtercondition1=CONTAINS&filteroperator1=1&filterdatafield1=securityTerm&filterscount=2&groupscount=0&pagenum=0&pagesize=2000&recordstartindex=0&recordendindex=2000'
+  json_string = open(url).read()
+  json = JSON.parse(json_string)
+  
+  # json is a Hash of the form:
+  # {"totalResultsCount"=>1833,
+  #  "securityList"=>
+  #   [
+  #     {
+  #       "cusip"=>"912796EE5",
+  #       "issueDate"=>"2014-08-21T00:00:00",
+  #       "securityType"=>"Bill",
+  #       "securityTerm"=>"13-Week",
+  #       "maturityDate"=>"2014-11-20T00:00:00",
+  #       "interestRate"=>"",
+  #       "refCpiOnIssueDate"=>"",
+  #       "refCpiOnDatedDate"=>"",
+  #       "announcementDate"=>"2014-08-14T00:00:00",
+  #       "auctionDate"=>"2014-08-18T00:00:00",
+  #       "auctionDateYear"=>"2014",
+  #       "datedDate"=>"",
+  #       "accruedInterestPer1000"=>"",
+  #       "accruedInterestPer100"=>"",
+  #       "adjustedAccruedInterestPer1000"=>"",
+  #       "adjustedPrice"=>"",
+  #       "allocationPercentage"=>"61.240000",
+  #       "allocationPercentageDecimals"=>"2",
+  #       "announcedCusip"=>"",
+  #       "auctionFormat"=>"Single-Price",
+  #       "averageMedianDiscountRate"=>"0.025000",
+  #       "averageMedianInvestmentRate"=>"",
+  #       "averageMedianPrice"=>"",
+  #       "averageMedianDiscountMargin"=>"",
+  #       "averageMedianYield"=>"",
+  #       "backDated"=>"",
+  #       "backDatedDate"=>"",
+  #       "bidToCoverRatio"=>"4.700000",
+  #       "callDate"=>"",
+  #       "callable"=>"",
+  #       "calledDate"=>"",
+  #       "cashManagementBillCMB"=>"No",
+  #       "closingTimeCompetitive"=>"11:30 AM",
+  #       "closingTimeNoncompetitive"=>"11:00 AM",
+  #       "competitiveAccepted"=>"28446079000",
+  #       "competitiveBidDecimals"=>"3",
+  #       "competitiveTendered"=>"135651339000",
+  #       "competitiveTendersAccepted"=>"Yes",
+  #       "corpusCusip"=>"",
+  #       "cpiBaseReferencePeriod"=>"",
+  #       "currentlyOutstanding"=>"23002000000.000000",
+  #       "directBidderAccepted"=>"1026942000",
+  #       "directBidderTendered"=>"6778302000",
+  #       "estimatedAmountOfPubliclyHeldMaturingSecuritiesByType"=>"114999000000",
+  #       "fimaIncluded"=>"Yes",
+  #       "fimaNoncompetitiveAccepted"=>"100000000",
+  #       "fimaNoncompetitiveTendered"=>"100000000",
+  #       "firstInterestPeriod"=>"",
+  #       "firstInterestPaymentDate"=>"",
+  #       "floatingRate"=>"No",
+  #       "frnIndexDeterminationDate"=>"",
+  #       "frnIndexDeterminationRate"=>"",
+  #       "highDiscountRate"=>"0.030000",
+  #       "highInvestmentRate"=>"0.030000",
+  #       "highPrice"=>"99.992417",
+  #       "highDiscountMargin"=>"",
+  #       "highYield"=>"",
+  #       "indexRatioOnIssueDate"=>"",
+  #       "indirectBidderAccepted"=>"12762957000",
+  #       "indirectBidderTendered"=>"13073037000",
+  #       "interestPaymentFrequency"=>"None",
+  #       "lowDiscountRate"=>"0.010000",
+  #       "lowInvestmentRate"=>"",
+  #       "lowPrice"=>"",
+  #       "lowDiscountMargin"=>"",
+  #       "lowYield"=>"",
+  #       "maturingDate"=>"2014-08-21T00:00:00",
+  #       "maximumCompetitiveAward"=>"10150000000",
+  #       "maximumNoncompetitiveAward"=>"5000000",
+  #       "maximumSingleBid"=>"10150000000",
+  #       "minimumBidAmount"=>"100",
+  #       "minimumStripAmount"=>"",
+  #       "minimumToIssue"=>"100",
+  #       "multiplesToBid"=>"100",
+  #       "multiplesToIssue"=>"100",
+  #       "nlpExclusionAmount"=>"8100000000",
+  #       "nlpReportingThreshold"=>"10150000000",
+  #       "noncompetitiveAccepted"=>"454078900",
+  #       "noncompetitiveTendersAccepted"=>"Yes",
+  #       "offeringAmount"=>"29000000000",
+  #       "originalCusip"=>"",
+  #       "originalDatedDate"=>"",
+  #       "originalIssueDate"=>"2014-05-22T00:00:00",
+  #       "originalSecurityTerm"=>"26-Week",
+  #       "pdfFilenameAnnouncement"=>"A_20140814_1.pdf",
+  #       "pdfFilenameCompetitiveResults"=>"R_20140818_2.pdf",
+  #       "pdfFilenameNoncompetitiveResults"=>"NCR_20140818_2.pdf",
+  #       "pdfFilenameSpecialAnnouncement"=>"",
+  #       "pricePer100"=>"99.992417",
+  #       "primaryDealerAccepted"=>"14656180000",
+  #       "primaryDealerTendered"=>"115800000000",
+  #       "reopening"=>"Yes",
+  #       "securityTermDayMonth"=>"91-Day",
+  #       "securityTermWeekYear"=>"13-Week",
+  #       "series"=>"",
+  #       "somaAccepted"=>"0",
+  #       "somaHoldings"=>"0",
+  #       "somaIncluded"=>"No",
+  #       "somaTendered"=>"0",
+  #       "spread"=>"",
+  #       "standardInterestPaymentPer1000"=>"",
+  #       "strippable"=>"",
+  #       "term"=>"13-Week",
+  #       "tiinConversionFactorPer1000"=>"",
+  #       "tips"=>"No",
+  #       "totalAccepted"=>"29000157900",
+  #       "totalTendered"=>"136205417900",
+  #       "treasuryDirectAccepted"=>"294886900",
+  #       "treasuryDirectTendersAccepted"=>"Yes",
+  #       "type"=>"Bill",
+  #       "unadjustedAccruedInterestPer1000"=>"",
+  #       "unadjustedPrice"=>"",
+  #       "updatedTimestamp"=>"2014-08-18T11:32:48",
+  #       "xmlFilenameAnnouncement"=>"A_20140814_1.xml",
+  #       "xmlFilenameCompetitiveResults"=>"R_20140818_2.xml",
+  #       "xmlFilenameSpecialAnnouncement"=>""
+  #     },
+  #   ...
+  #   ]
+  # }
+        
+  security_list = json["securityList"]
+  treasury_bills = security_list.map do |security|
+    auction_date = Date.parse(security["auctionDate"])
+    issue_date = Date.parse(security["issueDate"])
+    maturity_date = Date.parse(security["maturityDate"])
+    face_value = BigDecimal.new(100)
+    purchase_price = BigDecimal.new(security["pricePer100"])
+    high_price = BigDecimal.new(security["highPrice"])
+    low_price = BigDecimal.new(security["lowPrice"])
+    high_discount_rate = BigDecimal.new(security["highDiscountRate"])
+    low_discount_rate = BigDecimal.new(security["lowDiscountRate"])
+    maturity_in_days = BigDecimal.new(91)
+    TreasuryBill.new(auction_date, issue_date, maturity_date, face_value, purchase_price, high_price, low_price, high_discount_rate, low_discount_rate, maturity_in_days)
+  end
+  
   lines = ["Date,Value"]
-  YahooFinance.get_historical_quotes("^IRX", start_date, end_date) do |row|
-    # each row is of the form: [date (yyyy-mm-dd), open, high, low, close, volume, adj-close]
-    date, open, high, low, close, volume, adj_close = *row
-    lines << "#{date},#{high.to_f / 100.0}"
+  treasury_bills.each do |tbill|
+    auction_date = tbill.auction_date
+    # discount_rate = tbill.high_discount_rate / BigDecimal.new(100)    # tbill.high_discount_rate is a percentage, not a fractional value. To convert to a fractional value, divide this by 100
+    # computed_discount_rate_from_purchase_price = calculate_discount_rate(tbill.purchase_price, tbill.face_value, tbill.maturity_in_days)    # from 1979-12-28 to 1998-10-26, this calculation differs considerably from computed_discount_rate_from_high_price, but from 1998-11-02 to today, there is no difference; conclusion, use high_price instead of purchase_price in calculation of discount rate
+    computed_discount_rate_from_high_price = calculate_discount_rate(tbill.high_price, tbill.face_value, tbill.maturity_in_days)
+    
+    # puts auction_date
+    # puts discount_rate.to_s("F")
+    # puts computed_discount_rate_from_purchase_price.to_s("F")
+    # puts computed_discount_rate_from_high_price.to_s("F")
+    
+    date_string = auction_date.strftime("%Y-%m-%d")
+    # discount_rate_string = discount_rate.to_s("F")
+    # discount_rate_string = computed_discount_rate_from_purchase_price.to_s("F")
+    discount_rate_string = computed_discount_rate_from_high_price.to_s("F")
+    
+    lines << "#{date_string},#{discount_rate_string}"
   end
   File.write(TreasuryBillRateFilePath, lines.join("\n"))
+end
+
+# see https://www.treasurydirect.gov/instit/marketables/tbills/tbills.htm
+# and http://www.newyorkfed.org/aboutthefed/fedpoint/fed28.html
+# for the calculation of the discount rate
+# note: both site give the same equation, but the terms are re-arranged. Ultimately, they both lead to:
+# d = 360 * (1 - (PP / FV)) / M
+# where PP = purchase price, FV = face value, M = maturity in days
+One = BigDecimal.new(1)
+def calculate_discount_rate(purchase_price, face_value, maturity_in_days)
+  BigDecimal.new(360) * (One - (purchase_price / face_value)) / maturity_in_days
 end
 
 # create single CSV file containing all futures data
